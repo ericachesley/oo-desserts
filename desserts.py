e5834ba766d49d6906feca4989d7d4f951ff5768
Erica Chesley <erchesley@gmail.com>
2020-05-11 12:24:39 -0700

    Exercise instructions complete

--- desserts.py
+++ desserts.py
@@ -4,10 +4,54 @@
 class Cupcake:
     """A cupcake."""
 
+    cache = {}
+
+    def __init__(self, name, flavor, price):
+      self.name = name
+      self.flavor = flavor
+      self.price = price
+      self.qty = 0
+      Cupcake.cache[self.name] = self
+
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+    def add_stock(self, amount):
+
+      self.qty += int(amount)
+
+
+    def sell(self, amount):
+
+      if self.qty == 0:
+        print('Sorry, these cupcakes are sold out')
+        return
+
+      if self.qty >= amount:
+        self.qty -= amount
+      else:
+        self.qty = 0
+
+
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+
+      new_ingredients = []
+      for ingredient in ingredients:
+        new_ingredients.append((ingredient[0], ingredient[1]*amount))
+      return new_ingredients
+
+
+    @classmethod
+    def get(cls, name):
+      for cupcake in cls.cache:
+        if cupcake == name:
+          return cls.cache[cupcake]
+
+      print("Sorry, that cupcake doesn't exist")
+
 
 
 if __name__ == '__main__':
